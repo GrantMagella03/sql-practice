a41b2c0fd9c84eca6b1d5625f8dcafb3f0eb0bf6
Grant Magella <Bitman2003@gmail.com>
2023-08-14 12:28:22 -0400

    notes

---
+++ insert notes.sql
@@ -0,0 +1,25 @@
+/*INSERT Customers (Name, City, State, Sales, Active)
+VALUES ('MAX Technical Training','Cincinnati','OH',9999999.99,Default);*/
+/*update Customers Set 
+Sales +=100,
+active=0
+--select * from customers
+where Id in (36,37)
+;*/
+-- begin transaction
+-- rollback
+-- commit transaction
+/*
+insert customers(name,city,state)
+values ('Max','Mason','OH')
+*/
+/*
+delete Customers
+where id in (38)
+*/
+
+/*
+Select *
+from Customers
+ORDER BY ID DESC;
+*/
