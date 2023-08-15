36ce0a005020db05ab3eff5760b1bd4d469ee5c2
Grant Magella <Bitman2003@gmail.com>
2023-08-15 09:31:21 -0400

    notes

--- dbcreation.sql
+++ dbcreation.sql
@@ -10,17 +10,17 @@ create table Students(
     State Varchar(2) not null DEFAULT 'OH',
     Zipcode Varchar(5)
 );*/
+/*
 INSERT Students(Firstname, LastName, Phone, Address, city, State, Zipcode)
     Values('Grant','Magella','5138888698', NULL ,'Wyoming','OH','45215');
 
 alter table students
     add Active Bit not null default 1
-/*
 Create Table Technologies (
     ID INT PRIMARY KEY IDENTITY(1,1),
     Name varchar(30) not null,
     InstructionDays int not null default 5
-)*/
+)
 
 Create Table curriculum(
     ID INT PRIMARY KEY IDENTITY(1,1),
@@ -28,3 +28,4 @@ Create Table curriculum(
     Topic Varchar(50) NOT NULL,
     TechnologiesID int NOT NULL FOREIGN KEY references Technologies(ID)
 )
+*/
---
+++ index1.sql
@@ -0,0 +1,1 @@
+--create INDEX IDX_LastName ON STUDENTS (LastName asc);
